11451dcc6dcd1204562f13f9ee51e43cc87592c5
annabella-gt <113298859+annabella-gt@users.noreply.github.com>
2024-10-14 22:55:50 +0200

    Added JS and moment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
+  losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("Do MMMM YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  let barcelonaElement = document.querySelector("#barcelona");
+  let barcelonaDateElement = barcelonaElement.querySelector(".date");
+  let barcelonaTimeElement = barcelonaElement.querySelector(".time");
+
+  barcelonaTime = moment().tz("Europe/Madrid");
+  barcelonaDateElement.innerHTML = barcelonaTime.format("Do MMMM YYYY");
+  barcelonaTimeElement.innerHTML = barcelonaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
